08b72db2d79787a9f62251fc217062b0789649f3
Calvin <calvin@ty.pe.kr>
2015-04-08 00:25:17 +1200

    Clean up

--- der_tia.py
+++ der_tia.py
@@ -7,26 +7,29 @@ from scipy.stats.stats import pearsonr as pearson_correlation
 from sklearn.cluster import KMeans
 from sklearn.neighbors import NearestNeighbors
 
-def degsim(input_matrix, k):
+def degsim(matrix, k):
     """The average Pearson correlation of the `k` nearest neightbours.
     `k` is the number of neightbours.
     """
     assert k > 1
     neigh = NearestNeighbors(k)
 
-    neigh.fit(input_matrix)
+    neigh.fit(matrix)
     kneighbors = neigh.kneighbors(return_distance=False)
 
     def compute(hood):
-        return mean([pearson_correlation(input_matrix[x], input_matrix[y])[0] for x, y in combinations(hood, r=2)])
+        return mean([pearson_correlation(matrix[x], matrix[y])[0]
+            for x, y in combinations(hood, r=2)])
 
     return [compute(hood) for hood in kneighbors]
 
 
-def rdma(input_matrix):
+def rdma(matrix):
     """RDMA measures the deviation of agreement from other data on a set of
     target items, combining with the inverse rating frequency for these items.
     """
-    def calculate(row):
-        return mean([abs(value - mean(input_matrix[:,i])) / len(input_matrix[:,i]) for i, value in enumerate(row)])
-    return array([[calculate(row)] for row in input_matrix])
+    def compute(row):
+        return mean([abs(value - mean(matrix[:,i])) / len(matrix[:,i])
+            for i, value in enumerate(row)])
+
+    return array([[compute(row)] for row in matrix])
